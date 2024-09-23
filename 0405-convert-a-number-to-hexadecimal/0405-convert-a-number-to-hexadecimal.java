af37bd3d25ea1d6d95d622f6d4e0748407f5303f
anu-304 <anusria304@gmail.com>
2024-09-23 14:09:58 +0530

    Time: 0 ms (100%), Space: 41.1 MB (30.74%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -1,5 +1,25 @@
 class Solution {
     public String toHex(int num) {
-        return Integer.toHexString(num);
+       // return Integer.toHexString(num);
+       if(num==0) 
+       {
+        return "0";
+       }
+       StringBuilder s = new StringBuilder();
+       while(num!=0)
+       {
+        int last4 = num&15;
+        if(last4<10)
+        {
+            s.append(last4);
+        }
+        else
+        {
+            s.append((char)(last4 - 10 +'a'));
+        }
+        num>>>=4;
+       }
+       return s.reverse().toString();
+
     }
 }
