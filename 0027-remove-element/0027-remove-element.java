0e2ebb46eeedf4ce0ababdebe2d44ce1dfc4a2ef
anu-304 <anusria304@gmail.com>
2024-09-24 09:18:43 +0530

    Time: 0 ms (100%), Space: 41.7 MB (68.44%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -5,10 +5,23 @@ class Solution {
         {
             if(nums[i]!=val)
             {
-                nums[count]=nums[i];
+                nums[count]=nums[i];  //using another pointer to change in-place
                 count++;
             }
         }
         return count;
+
+        /*
+        int k=0;
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                arr[k++]=nums[i];
+            }
+        }
+        //System.out.println(al);
+        return k;*/ //wrong output
     }
 }
