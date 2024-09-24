4968e5c8e1056745bf7e3b33c6377097f8a68844
anu-304 <anusria304@gmail.com>
2024-09-24 10:09:09 +0530

    Time: 17 ms (40.4%), Space: 59.1 MB (15.07%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        /*Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.get(nums[i])>=2)
+            {
+                return true;
+            }
+        
+        }
+        return false;*/
+        Set<Integer> s=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            s.add(nums[i]);
+        }
+        if(nums.length==s.size())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+}
