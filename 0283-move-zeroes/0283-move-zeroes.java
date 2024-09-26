407cf7b4bda50850116efff9b50d05860eab19ad
anu-304 <anusria304@gmail.com>
2024-09-26 09:31:43 +0530

    Time: 2 ms (84.11%), Space: 46.2 MB (11.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
+        /*int n=nums.length;
         int k=0;
         for(int i=0;i<n;i++)
         {
             if(nums[i]!=0)
             {
-                nums[k]=nums[i];
+                nums[k]=nums[i];  
                 k++;
             }
         }
@@ -14,9 +14,26 @@ class Solution {
         {
             nums[k]=0;
             k++;
-        }
+        }*/
+         //2 pointers
+
+         int l=0,r=0;
+         int t=0;
+         for(int i=0;i<nums.length;i++)
+         {
+            if(nums[r]!=0)
+            {
+                t=nums[r];
+                nums[r]=nums[l];
+                nums[l]=t;
+                l++;
+            }
+            r++;
+         }
            
 
     }
 }                 //the non zero element is stored and the pointer is increamented
                   // k pointer points a new array(imagine)
+                  //O(n) -time complexity
+                  //O(1) -space complextiy
