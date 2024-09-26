dffb340cac0a8f18555bd28d056db532b82d6a4f
anu-304 <anusria304@gmail.com>
2024-09-26 16:19:04 +0530

    Time: 3 ms (74.11%), Space: 41.2 MB (97.39%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -2,7 +2,7 @@ class Solution {
     public int longestPalindrome(String s) {
         //convert to character array
         //set 
-        
+        /*
         Set<Character> a=new HashSet<>();
         int l=0;
         for(char ch:s.toCharArray())
@@ -21,6 +21,38 @@ class Solution {
         {
             l+=1;
         }
-        return l;
+        return l; */
+        int[] arr =new int[52];
+        for(char ch:s.toCharArray())
+        {
+            if(Character.isUpperCase(ch))
+            {
+                arr[ch-'A']++;
+            }
+            else
+            {
+                arr[ch-'a'+26]++;
+            }
+        }
+        int count=0;
+        int odd=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                count=count+arr[i];
+            }
+
+            else if(arr[i]%2==1 && arr[i]!=1)
+            {
+                count= count+arr[i]-1;
+                odd=1;
+            }
+            else if(arr[i]%2==1 && arr[i]==1)
+            {
+                odd=1;
+            }
+        }
+        return count+odd;
     }
 }
