4948d81dbc8b50efb974a7c3802d9f541647f3d7
anu-304 <anusria304@gmail.com>
2024-09-26 10:00:15 +0530

    Time: 3 ms (88.92%), Space: 43.1 MB (65.89%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int k=0;
+        /*int k=0;
         ArrayList<Integer> al=new ArrayList<>();
         for(int i=0;i<nums1.length;i++)
         {
@@ -20,6 +20,33 @@ class Solution {
         {
             res[i]=al.get(i);
         }
+        return res; */
+        int l1=nums1.length;
+        int l2=nums2.length;
+        int l=Math.min(l1,l2);
+        int counter=0;
+        int[] ans=new int[l];
+        for(int i=0;i<nums1.length;i++)
+        {
+            for(int j=0;j<nums2.length;j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    ans[counter]=nums1[i];
+                    counter++;
+                    nums2[j]=Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+        int[] res=new int[counter];
+        for(int i=0;i<counter;i++)
+        {
+            if(ans[i]!=0){
+            res[i]=ans[i];
+            }
+        }
         return res;
-    }
+
+    } 
 }
