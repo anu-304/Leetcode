37516378ae74f7c5b060a713d0644f4c9de4360a
anu-304 <anusria304@gmail.com>
2024-09-27 10:17:44 +0530

    Time: 15 ms (7.75%), Space: 45.2 MB (45.18%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countBinarySubstrings(String s) {
-        int ans=0;
+       /* int ans=0;
         int prev=0,curr=1;
         for(int i=1;i<s.length();i++)
         {
@@ -18,6 +18,33 @@ class Solution {
         }
         ans=ans+Math.min(prev,curr);
         return ans;
+        */
+        ArrayList<Integer> al=new ArrayList<>();
+        int c=1;
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i)==s.charAt(i-1))
+            {
+                c++;
+            }
+            else
+            {
+                al.add(c);
+                c=1;
+            }
+        }
+        al.add(c);
+            
+            
+        int res=0;
+        for(int i=1;i<al.size();i++)
+        {
+            res+=Math.min(al.get(i-1),al.get(i));
+        }
+        return res;
+        
+        
     }
+
 
 }
