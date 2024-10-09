0aaf4322dc304b05fe5dd5366bcf4a67436fc175
anu-304 <anusria304@gmail.com>
2024-10-09 12:03:55 +0530

    Time: 6 ms (97.73%), Space: 45.2 MB (90.35%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> {
+            return (a[0] == b[0])? a[1] - b[1] : b[0] - a[0];
+        });
+
+        List<int[]> result = new ArrayList<>();
+
+        for(int[] person:people){
+            result.add(person[1], person);
+        }
+
+        return result.toArray(new int[people.length][]);
+    }
+}
