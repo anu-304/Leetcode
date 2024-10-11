3a53499533c23365ff6a8feedd12d0c1d9d384e7
anu-304 <anusria304@gmail.com>
2024-10-11 05:31:21 +0530

    Time: 0 ms (100%), Space: 48.9 MB (54.48%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,16 +1,16 @@
 class Solution {
     public void reverseString(char[] s) {
+        int n=s.length;
         int l=0;
-        int r=s.length-1;
-        char temp;
+        int r=n-1;
         while(l<r)
         {
-            temp=s[l];
+            char temp=s[l];
             s[l]=s[r];
             s[r]=temp;
             l++;
             r--;
         }
-        
+       
     }
 }
