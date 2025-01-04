0cc3801beb1be8616ac5afdfbda1266cadfd44cd
anu-304 <anusria304@gmail.com>
2025-01-04 09:16:29 +0530

    Time: 2 ms (100%), Space: 57 MB (97.11%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -1,23 +1,20 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
         int n=nums.length;
-        long sumTotal=0;
-        long sumCurr=0;
-        int cnt=0;
-
-        // Calculate total sum
-        for(int i=0; i<n; i++){
-            sumTotal+=nums[i];
+        long currsum=0;
+        long totalsum=0;
+        int c=0;
+        for(int i=0;i<n;i++){
+            totalsum+=nums[i];
         }
 
-        // Check valid splits condition
-        for(int i=0; i<n-1; i++){
-            sumCurr+=nums[i];
-            if(sumCurr >= sumTotal-sumCurr){
-                cnt++;
+        for(int i=0;i<n-1;i++)
+        {
+            currsum+=nums[i];
+            if(currsum>=totalsum-currsum){
+                c++;
             }
         }
-
-        return cnt;
+        return c;
     }
 }
