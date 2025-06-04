2bcb31bbefba5e3ebee6466dcbf6fc601afd60f2
Anusri A <anusria304@gmail.com>
2025-06-04 15:28:43 +0530

    Time: 13 ms (32.19%), Space: 45.2 MB (7.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,47 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        /*int c=0;
-        char[] ss=s.toCharArray();      
-        char[] ts=t.toCharArray(); 
-        Arrays.sort(ss);
-        Arrays.sort(ts);
-        for(int i=0;i<s.length();i++)
-        {
-            if(ss[i]!=ts[i])
-            {
-                break;
-            }
-            else{
-            c++;
-            }
-        }
-        if(c==s.length()) return true;
-        else return false;*/
-
-        Map<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
+        Map<Character,Integer> map = new HashMap<>();
+        for(char ch:s.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=t.charAt(i);
-            if(map.containsKey(ch))
-            {
-                map.put(ch,map.getOrDefault(ch,0)-1);
-            }
+        for(char ch:t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)-1);
         }
-        
-        for(int i=0;i<s.length();i++)
-        {
-            if(map.get(s.charAt(i))!=0)
-            {
+        for(var a:map.entrySet()){
+            if(a.getValue()!=0){
                 return false;
             }
         }
         return true;
-        
     }
 }
