f9766dda6b3d7fdda6f570f76494f55a5465c26c
Anusri A <anusria304@gmail.com>
2025-06-04 20:53:26 +0530

    Time: 0 ms (100%), Space: 42.1 MB (6.19%) - LeetHub

---
+++ 0961-long-pressed-name/0961-long-pressed-name.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int n=name.length();
+        int m=typed.length();
+        int i=0,j=0;
+        if(n>m) return false;
+        if(name.charAt(0) != typed.charAt(0)) return false;
+        while(i<n && j<m){
+            if(name.charAt(i) == typed.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else if(typed.charAt(j) == name.charAt(i-1)){
+                j++;
+            }
+            else{
+                return false;
+            }
+        }
+        while(j<m){
+            if(name.charAt(i-1) == typed.charAt(j)){
+                j++;
+            }
+            else{
+                return false;
+            }
+        }
+        if(i<n) return false;
+        return true;
+    }
+}
