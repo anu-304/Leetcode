142ae489e6f76dae301109eed926caac189df5a8
Anusri A <anusria304@gmail.com>
2025-07-20 14:53:45 +0530

    Time: 2 ms (80.35%), Space: 46 MB (78.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,39 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        /*int n=nums.length;
         int k=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=0)
-            {
-                nums[k]=nums[i];  
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[k]=nums[i];
                 k++;
+                //i++;
             }
         }
-        while(k<n)
-        {
-            nums[k]=0;
-            k++;
-        }*/
-         //2 pointers
-
-         int l=0,r=0;
-         int t=0;
-         for(int i=0;i<nums.length;i++)
-         {
-            if(nums[r]!=0)
-            {
-                t=nums[r];
-                nums[r]=nums[l];
-                nums[l]=t;
-                l++;
-            }
-            r++;
-         }
-           
-
+        for(int i=k;i<nums.length;i++){
+            nums[i]=0;
+        }
     }
-}                 //the non zero element is stored and the pointer is increamented
-                  // k pointer points a new array(imagine)
-                  //O(n) -time complexity
-                  //O(1) -space complextiy
+}
