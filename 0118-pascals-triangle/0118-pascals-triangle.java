609b1f781556d19ca658171a815344bdaef0423b
Anusri A <anusria304@gmail.com>
2025-07-26 08:04:28 +0530

    Time: 0 ms (100%), Space: 42.3 MB (24.33%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public static List<Integer> generateRow(int n){
+        List<Integer> temp = new ArrayList<>();
+        long ans=1;
+        temp.add(1);
+        for(int i=1;i<n;i++){
+            ans=ans*(n-i);
+            ans=ans/i;
+            temp.add((int)ans);
+        }
+        return temp;
+    }
+
+    public List<List<Integer>> func(int n){
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            ans.add(generateRow(i));
+        }
+        return ans;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = func(numRows);
+        return ans;
+    }
+}
