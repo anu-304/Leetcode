7ff47a086faa420f766087c6a97e7845c462121d
Anusri A <anusria304@gmail.com>
2025-07-26 10:10:14 +0530

    Time: 0 ms (100%), Space: 43.2 MB (36.31%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        ArrayList<Integer> al = new ArrayList<>();
-        for(int num:nums){
-            al.add(num);
-        }
-
         int idx = -1;
         int n=nums.length;
         for(int i=n-2;i>=0;i--){
-            if(al.get(i)<al.get(i+1)){
+            if(nums[i]<nums[i+1]){
                 idx=i;
                 break;
             }
         }
 
         if(idx == -1){
-            Collections.reverse(al);
-            for(int i=0;i<n;i++){
-                nums[i]=al.get(i);
-            }
+            reverse(nums,0,n-1);
             return;
         }
         int t=0;
         for(int i=n-1;i>=0;i--){
-            if(al.get(i)>al.get(idx)){
-                t=al.get(i);
-                al.set(i,al.get(idx));
-                al.set(idx,t);
+            if(nums[i]>nums[idx]){
+                swap(nums,i,idx);
                 break;
             }
         }
-        List<Integer> sub = al.subList(idx+1,n);
-        Collections.reverse(sub);
+        reverse(nums, idx+1, n-1);
+    }
 
-        for(int i=0;i<n;i++){
-            nums[i]=al.get(i);
+    public static void swap(int[] nums,int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+
+    public static void reverse(int[] nums, int start, int end){
+        while(start<=end){
+            swap(nums, start++, end--);
         }
     }
 }
