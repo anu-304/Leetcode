5b87b1eb650b9602b25299f7227bcff43ff911f8
Anusri A <anusria304@gmail.com>
2025-07-31 07:40:29 +0530

    Time: 77 ms (62.81%), Space: 46.6 MB (61.11%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,51 +1,37 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>(wordList);
-        if (!wordSet.contains(endWord)) return 0;
+    public int ladderLength(String startWord, String targetWord, List<String> wordList) {
+        Queue<Pair<String,Integer>> q=new LinkedList<>();
+        q.add(new Pair<>(startWord,1));
 
-        Queue<String> queue = new LinkedList<>();
-        queue.offer(beginWord);
-        Set<String> visited = new HashSet<>();
-        visited.add(beginWord);
+        Set<String> st = new HashSet<>(wordList);
+        st.remove(startWord);
 
-        int length = 1;
-        
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            for (int i = 0; i < levelSize; i++) {
-                String currentWord = queue.poll();
-                if (currentWord.equals(endWord)) return length;
+        while(!q.isEmpty()){
+            Pair<String, Integer> node = q.poll();
+            String word = node.getKey();
+            int steps = node.getValue();
 
-                for (String neighbor : getNeighbors(currentWord, wordSet)) {
-                    if (!visited.contains(neighbor)) {
-                        visited.add(neighbor);
-                        queue.offer(neighbor);
+            if(word.equals(targetWord)) return steps;
+
+            for(int i=0;i<word.length();i++){
+                char[] wordChars = word.toCharArray();
+                char orginialChars = wordChars[i];
+
+                for(char ch='a'; ch<='z';ch++){
+                    wordChars[i]=ch;
+                    String newWord = new String(wordChars);
+
+                    if(st.contains(newWord)){
+                        st.remove(newWord);
+                        q.add(new Pair<>(newWord, steps+1));
                     }
                 }
+                wordChars[i]=orginialChars;
+                
             }
-            length++;
+            
         }
-        
         return 0;
     }
-
-    private List<String> getNeighbors(String word, Set<String> wordSet) {
-        List<String> neighbors = new ArrayList<>();
-        char[] wordChars = word.toCharArray();
-
-        for (int i = 0; i < wordChars.length; i++) {
-            char originalChar = wordChars[i];
-            for (char c = 'a'; c <= 'z'; c++) {
-                if (c == originalChar) continue;
-                wordChars[i] = c;
-                String transformedWord = new String(wordChars);
-                if (wordSet.contains(transformedWord)) {
-                    neighbors.add(transformedWord);
-                }
-            }
-            wordChars[i] = originalChar;
-        }
-        
-        return neighbors;
-    }
+    
 }
