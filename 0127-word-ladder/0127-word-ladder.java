d5023bb7fd90007957b5c574c849412537fc10d0
Anusri A <anusria304@gmail.com>
2025-08-01 11:20:29 +0530

    Time: 74 ms (68.98%), Space: 46.7 MB (53.49%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,37 +1,33 @@
 class Solution {
-    public int ladderLength(String startWord, String targetWord, List<String> wordList) {
-        Queue<Pair<String,Integer>> q=new LinkedList<>();
-        q.add(new Pair<>(startWord,1));
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair<String,Integer>> q = new LinkedList<>();
+        Set<String> set = new HashSet<>(wordList);
+        
 
-        Set<String> st = new HashSet<>(wordList);
-        st.remove(startWord);
+        q.add(new Pair<>(beginWord,1));
 
         while(!q.isEmpty()){
-            Pair<String, Integer> node = q.poll();
+            Pair<String,Integer> node=q.poll();
             String word = node.getKey();
             int steps = node.getValue();
-
-            if(word.equals(targetWord)) return steps;
+            if(word.equals(endWord)) return steps;
 
             for(int i=0;i<word.length();i++){
-                char[] wordChars = word.toCharArray();
-                char orginialChars = wordChars[i];
+                char[] charArray = word.toCharArray();
+                char org = charArray[i];
 
-                for(char ch='a'; ch<='z';ch++){
-                    wordChars[i]=ch;
-                    String newWord = new String(wordChars);
+                for(char ch='a';ch<='z';ch++){
+                    charArray[i]=ch;
+                    String newWord = new String(charArray);
 
-                    if(st.contains(newWord)){
-                        st.remove(newWord);
+                    if(set.contains(newWord)){
                         q.add(new Pair<>(newWord, steps+1));
+                        set.remove(newWord);
                     }
                 }
-                wordChars[i]=orginialChars;
-                
+                charArray[i]=org;
             }
-            
         }
         return 0;
     }
-    
 }
