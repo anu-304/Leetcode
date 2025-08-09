3350462db5eac75b99fa9e4e99dd6c9bb68148a9
Anusri A <anusria304@gmail.com>
2025-08-09 07:01:15 +0530

    Time: 0 ms (100%), Space: 44.4 MB (90.25%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -2,24 +2,19 @@ class Solution {
     public int[][] flipAndInvertImage(int[][] matrix) {
 
         int n=matrix.length;
-        int[][] res = new int[n][n];
+        
         for(int i=0;i<matrix.length;i++){
-            int l=0;
-            int r=matrix.length - 1;
-            while(l<r){
-                int t = matrix[i][l];
-                matrix[i][l] = matrix[i][r];
-                matrix[i][r] = t;
-                l++;
-                r--;
+            
+            int c=matrix[0].length - 1;
+            for(int j=0;j<=c;j++,c--){
+                int t = 1 - matrix[i][j];
+                matrix[i][j] = 1- matrix[i][c];
+                matrix[i][c] = t;
+                
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                res[i][j] = 1-matrix[i][j];
-            }
-        }
-        return res;
+        
+        return matrix;
     }
 }
