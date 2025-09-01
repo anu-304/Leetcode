ea0bf3171098c35a420105f9eca76a914b0dc073
Anusri A <anusria304@gmail.com>
2025-09-01 13:04:13 +0530

    Time: 0 ms (100%), Space: 41.9 MB (68.18%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,27 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]!=val)
-            {
-                nums[count]=nums[i];  //using another pointer to change in-place
-                count++;
-            }
-        }
-        return count;
-
-        /*
         int k=0;
-        int[] arr=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]!=val)
-            {
-                arr[k++]=nums[i];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
             }
         }
-        //System.out.println(al);
-        return k;*/ //wrong output
+        return k;
     }
 }
