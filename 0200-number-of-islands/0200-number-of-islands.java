2d2d61e12da553604a965b7d0318857bd4e55ecc
Anusri A <anusria304@gmail.com>
2025-09-02 06:19:17 +0530

    Time: 3 ms (83.77%), Space: 49.3 MB (81.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,35 +1,40 @@
 class Solution {
 
-    public void mark0(char[][] grid, int i,int j){
+
+    public void mark0(char[][] grid, int i, int j){
         int r = grid.length;
         int c = grid[0].length;
-        if(i<0 || j<0)
+
+        if(i<0 || j<0){
             return;
+        }
         if(i>=r || j>=c)
             return;
-        if(grid[i][j] == '0')
+        if(grid[i][j]=='0') 
             return;
+        
+        grid[i][j]='0';
 
-        grid[i][j] = '0';
-
-        mark0(grid, i-1, j);
-        mark0(grid, i+1, j);
-        mark0(grid, i, j-1);
-        mark0(grid, i, j+1);
+        mark0(grid,i+1,j);
+        mark0(grid,i-1,j);
+        mark0(grid,i,j+1);
+        mark0(grid,i,j-1);
     }
 
     public int numIslands(char[][] grid) {
-        int r = grid.length;
+        int r= grid.length;
         int c = grid[0].length;
         int count = 0;
-        for(int i=0; i<r; i++){
+        for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j] == '1'){
+                if(grid[i][j]=='1'){
                     count++;
-                    mark0(grid, i, j);
+                    mark0(grid,i,j);
                 }
             }
         }
         return count;
+
+
     }
 }
