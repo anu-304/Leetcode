93655958b34e8ad19b884665c83c50af0e15e812
Anusri A <anusria304@gmail.com>
2025-09-02 05:58:44 +0530

    Time: 78 ms (59.32%), Space: 47.2 MB (34.96%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,30 +1,31 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Queue<Pair<String, Integer>> q = new LinkedList<>();
-        Set<String> set = new HashSet<>(wordList);
-        q.add(new Pair<>(beginWord,1));
+        HashSet<String> hs = new HashSet<>(wordList);
+
+        q.add(new Pair<>(beginWord, 1));
 
         while(!q.isEmpty()){
             Pair<String, Integer> node = q.poll();
             String word = node.getKey();
             int steps = node.getValue();
-
-            if(word.equals(endWord)) return steps;
+            if(word.equals(endWord)){
+                return steps;
+            }
 
             for(int i=0;i<word.length();i++){
-                char[] wordArray = word.toCharArray();
-                char org = wordArray[i];
+                char[] charArr = word.toCharArray();
+                char org = charArr[i];
 
-                for(char ch='a';ch<='z';ch++){
-                    wordArray[i]=ch;
-                    String newWord = new String(wordArray);
-                    if(set.contains(newWord)){
-                        set.remove(newWord);
+                for(char ch = 'a'; ch<='z';ch++){
+                    charArr[i]=ch;
+                    String newWord = new String(charArr);
+                    if(hs.contains(newWord)){
+                        hs.remove(newWord);
                         q.add(new Pair<>(newWord, steps+1));
                     }
                 }
-
-                wordArray[i]=org;
+                charArr[i]=org;
             }
         }
         return 0;
